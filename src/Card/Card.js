e9e2dfb9aa2ef60f604ba4b5ba7a8a12f9162c95
Patt Sookmark <pijiya@sookmark.com>
2023-02-21 11:11:42 -0700

    Add eventhandler to input field

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -4,7 +4,7 @@ import React from "react";
 const Card = ({ name, date, reserveTime, numGuest }) => {
   return (
     <div className="card">
-      <p>{name}</p>
+      <h3>{name}</h3>
       <p>{date}</p>
       <p>{reserveTime}</p>
       <p>number of guest: {numGuest}</p>
--- src/form/form.js
+++ src/form/form.js
@@ -3,6 +3,45 @@ import "./Form.css";
 
 const Form = () => {
   //usestate input field
+  const [inputInfo, SetInputInfo] = useState({
+    name: "",
+    date: "",
+    reserveTime: "",
+    numGuest: "",
+  });
+  const handleChange = (event) => {
+    SetInputInfo({ ...inputInfo, [event.target.name]: event.target.value });
+  };
 
-  return <form></form>;
+  return (
+    <form>
+      <input
+        className="name"
+        type="text"
+        placeholder="Name"
+        onChange={(event) => handleChange(event)}
+      ></input>
+      <input
+        className="date"
+        type="text"
+        placeholder="Date (mm/dd)"
+        onChange={(event) => handleChange(event)}
+      ></input>
+      <input
+        className="reserveTime"
+        type="text"
+        placeholder="Time (7:00)"
+        onChange={(event) => handleChange(event)}
+      ></input>
+      <input
+        className="num-guest"
+        type="number"
+        placeholder="Number of guests"
+        onChange={(event) => handleChange(event)}
+      ></input>
+      <button>Make Reservation</button>
+    </form>
+  );
 };
+
+export default Form;
