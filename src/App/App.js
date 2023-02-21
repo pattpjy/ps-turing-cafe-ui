3002185b9f09a080e2b03a691ac68ed04f616447
Patt Sookmark <pijiya@sookmark.com>
2023-02-21 10:48:38 -0700

    Setup Form

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import AllReservation from "../AllRese/AllRese";
 import "./App.css";
+import Form from "../form/Form";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1 className="app-title">Turing Cafe Reservations</h1>
-        <div className="resy-form"></div>
+        <div className="resy-form">
+          <Form />
+        </div>
         <div className="resy-container">
           <AllReservation />
         </div>
